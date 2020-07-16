6e5fd5f028b4ee23a3e864dcc3f9b4bca8c895cc
brunooomelo <utibmelo@gmail.com>
2020-07-16 12:00:44 -0300

    finish commands

--- src/commands/moveall.js
+++ src/commands/moveall.js
@@ -7,8 +7,31 @@ module.exports = {
 		}
 	},
 	async run(client, message, args) {
+		const channel = client.guilds.get(process.env.GUILD_ID).channels.find((cn) => {
+			return (cn.id === args[0])
+		})
 
-		
+		if (!channel) {
+			throw new Error('no_have_channel')
+		}
+		const mentions = message.mentions.roles
+
+		const hasRole = (roles, user) => {
+			return roles.some((r) => {
+				return user.roles.find((UR) => UR.id === r.id )
+			})
+		}
+
+		const allmembers = client.guilds.get(process.env.GUILD_ID).members.filter(m => {
+			return m.voiceChannelID && !m.user.bot && m.voiceChannelID !== process.env.AFK && m.voiceChannelID !== channel.id && hasRole(mentions, m)
+		})
+
+		await Promise.all(allmembers.map((am) => {
+			return am.setVoiceChannel(channel.id)
+		}))
+
+		message.channel
+			.send('``✅`` Todos movidos para a Sala: '+ channel.name)
 	},
 
 	get command() {
@@ -16,7 +39,7 @@ module.exports = {
 			name: 'move-all',
 			category: categories.ADM,
 			description: 'Manda mensagem pra todos os usuários',
-			usage: 'move-all',
+			usage: 'move-all <CHANNEL> ...<MENTIONS>',
 		};
 	},
 };
